e947ba6afbe94d6fafe28d47845cc7dad1702c81
Bhavik <15388512+bhavik-knight@users.noreply.github.com15388512+bhavik-knight@users.noreply.github.com>
2023-05-06 18:10:40 -0300

    added some new icons

--- react/frontend/src/components/Icons.jsx
+++ react/frontend/src/components/Icons.jsx
@@ -1,15 +1,16 @@
-import { Image } from "@chakra-ui/react"
+// top navbar
+import { FaGithub } from "@react-icons/all-files/fa/FaGithub"
+import { FaLinkedinIn } from "@react-icons/all-files/fa/FaLinkedinIn"
+
+// side-navbar
 import { FaHome } from "@react-icons/all-files/fa/FaHome"
 import { FaTools } from "@react-icons/all-files/fa/FaTools"
 import { GiFilmProjector } from "@react-icons/all-files/gi/GiFilmProjector"
 import { MdSchool } from "@react-icons/all-files/md/MdSchool"
-import { GrUserWorker } from "@react-icons/all-files/gr/GrUserWorker"
-import { FaLinkedinIn } from "@react-icons/all-files/fa/FaLinkedinIn"
-import { FaGithub } from "@react-icons/all-files/fa/FaGithub"
 import { MdWork } from "@react-icons/all-files/md/MdWork"
 import { FaAward } from "@react-icons/all-files/fa/FaAward"
+import { GrUserWorker } from "@react-icons/all-files/gr/GrUserWorker"
 import { ImLinkedin2 } from "@react-icons/all-files/im/ImLinkedin2"
-
 
 // skills - technologies
 import { FaPython } from "@react-icons/all-files/fa/FaPython"
@@ -17,17 +18,22 @@ import { FaHtml5 } from "@react-icons/all-files/fa/FaHtml5"
 import { FaCss3 } from "@react-icons/all-files/fa/FaCss3"
 import { FaBootstrap } from "@react-icons/all-files/fa/FaBootstrap"
 import { SiJavascript } from "@react-icons/all-files/si/SiJavascript"
+import { SiJquery } from "@react-icons/all-files/si/SiJquery"
 import { FaJava } from "@react-icons/all-files/fa/FaJava"
 import { FaPhp } from "@react-icons/all-files/fa/FaPhp"
+import { FaGitAlt } from "@react-icons/all-files/fa/FaGitAlt"
+import { SiAmazonaws } from "@react-icons/all-files/si/SiAmazonaws"
+import { SiHeroku } from "@react-icons/all-files/si/SiHeroku"
+import { FaDocker } from "@react-icons/all-files/fa/FaDocker"
 
-const size = "64px"
+const size = { base: "md", sm: "64px" }
 
 const icons = {
     "home": <FaHome fontSize={size} />,
     "skills": <FaTools fontSize={size} />,
     "projects": <GiFilmProjector fontSize={size} />,
     "education": <MdSchool fontSize={size} />,
-    "experience": <MdWork fontSize={size} />,
+    "experiences": <MdWork fontSize={size} />,
     "certificates": <FaAward fontSize={size} />,
     "linkedin": <FaLinkedinIn fontSize={size} />,
     "github": <FaGithub fontSize={size} />,
@@ -36,25 +42,13 @@ const icons = {
     "python": <FaPython fontSize={size} />,
     "java": <FaJava fontSize={size} />,
     "javascript": <SiJavascript fontSize={size} />,
+    "jquery": <SiJquery fontSize={size} />,
     "bootstrap": <FaBootstrap fontSize={size} />,
-    "php": <FaPhp fontSize={size} />
-}
-
-export {
-    FaHome,
-    FaTools,
-    GiFilmProjector,
-    MdSchool,
-    GrUserWorker,
-    FaLinkedinIn,
-    FaGithub,
-    MdWork,
-    FaAward,
-    ImLinkedin2,
-    FaPython,
-    FaJava,
-    FaHtml5,
-    FaCss3,
+    "php": <FaPhp fontSize={size} />,
+    "git": <FaGitAlt fontSize={size} />,
+    "aws": <SiAmazonaws fontSize={size} />,
+    "heroku": <SiHeroku fontSize={size} />,
+    "docker": <FaDocker fontSize={size} />,
 }
 
 export { icons }
