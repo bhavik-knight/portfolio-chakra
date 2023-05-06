73e6988460ff14196722c123e84ab982d8f71cf9
Bhavik <15388512+bhavik-knight@users.noreply.github.com15388512+bhavik-knight@users.noreply.github.com>
2023-05-06 17:43:00 -0300

    revamping  the icons by creating an icon object instead of exporting one by one

--- react/frontend/src/components/Icons.jsx
+++ react/frontend/src/components/Icons.jsx
@@ -10,12 +10,35 @@ import { MdWork } from "@react-icons/all-files/md/MdWork"
 import { FaAward } from "@react-icons/all-files/fa/FaAward"
 import { ImLinkedin2 } from "@react-icons/all-files/im/ImLinkedin2"
 
-export const certificateIcon = <Image
-    src="certificate_icon.png"
-    boxSize="16px"
-    alt="certificateIcon"
-    color="white"
-/>
+
+// skills - technologies
+import { FaPython } from "@react-icons/all-files/fa/FaPython"
+import { FaHtml5 } from "@react-icons/all-files/fa/FaHtml5"
+import { FaCss3 } from "@react-icons/all-files/fa/FaCss3"
+import { FaBootstrap } from "@react-icons/all-files/fa/FaBootstrap"
+import { SiJavascript } from "@react-icons/all-files/si/SiJavascript"
+import { FaJava } from "@react-icons/all-files/fa/FaJava"
+import { FaPhp } from "@react-icons/all-files/fa/FaPhp"
+
+const size = "64px"
+
+const icons = {
+    "home": <FaHome fontSize={size} />,
+    "skills": <FaTools fontSize={size} />,
+    "projects": <GiFilmProjector fontSize={size} />,
+    "education": <MdSchool fontSize={size} />,
+    "experience": <MdWork fontSize={size} />,
+    "certificates": <FaAward fontSize={size} />,
+    "linkedin": <FaLinkedinIn fontSize={size} />,
+    "github": <FaGithub fontSize={size} />,
+    "html": <FaHtml5 fontSize={size} />,
+    "css": <FaCss3 fontSize={size} />,
+    "python": <FaPython fontSize={size} />,
+    "java": <FaJava fontSize={size} />,
+    "javascript": <SiJavascript fontSize={size} />,
+    "bootstrap": <FaBootstrap fontSize={size} />,
+    "php": <FaPhp fontSize={size} />
+}
 
 export {
     FaHome,
@@ -28,4 +51,10 @@ export {
     MdWork,
     FaAward,
     ImLinkedin2,
+    FaPython,
+    FaJava,
+    FaHtml5,
+    FaCss3,
 }
+
+export { icons }
