70777500054947a53691211efd232f95da976bee
Bhavik <15388512+bhavik-knight@users.noreply.github.com15388512+bhavik-knight@users.noreply.github.com>
2023-05-08 02:45:34 -0300

    added Home.css, Home.jsx to be consistent with naming; replacement of about

---
+++ react/frontend/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react"
+import { Container, useBoolean, useMediaQuery, withDefaultColorScheme } from "@chakra-ui/react"
+
+function Home() {
+    return (
+        <Container mt={40}>
+            about me
+        </Container>
+    )
+}
+
+export { Home }
