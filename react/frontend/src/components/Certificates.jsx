9557e6027e882b56a9b5972d4004fd65f747b407
Bhavik <15388512+bhavik-knight@users.noreply.github.com15388512+bhavik-knight@users.noreply.github.com>
2023-05-09 18:57:13 -0300

    vite.svg path updated to resolve the icon error

--- react/frontend/src/components/Certificates.jsx
+++ react/frontend/src/components/Certificates.jsx
@@ -1,8 +1,13 @@
+import { Flex, Box, Container, Wrap, Stack } from "@chakra-ui/react"
+import { Accordion, AccordionButton, AccordionItem, AccordionIcon, AccordionPanel } from "@chakra-ui/react"
 
-function Certificates() {
+
+function Accomplishments() {
     return (
-        <div>Certificates</div>
+        <Flex>
+            Throughout my non-traditional journey in the Computer Science field I have completed several courses and certification to breakout into the industry. Checkout my certifications that I obtained in different domains from various institutions.
+        </Flex>
     )
 }
 
-export { Certificates }
+export { Accomplishments }
