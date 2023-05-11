229dedc4b485c11ebde327c55a7b9a7aed8ead64
Bhavik <15388512+bhavik-knight@users.noreply.github.com15388512+bhavik-knight@users.noreply.github.com>
2023-05-11 04:34:20 -0300

    completed: the design of the experience page

---
+++ react/frontend/src/components/ExperienceCard.jsx
@@ -0,0 +1,48 @@
+import { nanoid } from "nanoid"
+import { Accordion, AccordionButton, AccordionItem, AccordionIcon, AccordionPanel } from "@chakra-ui/react"
+import { Text, Heading } from "@chakra-ui/react"
+import { Flex, Divider, Stack, Spacer } from "@chakra-ui/react"
+import { List, UnorderedList, ListItem } from "@chakra-ui/react"
+
+function ExperienceCard({ job }) {
+    return (
+        <AccordionItem py={2}>
+            <AccordionButton
+                as={Heading}
+                fontSize={{ base: "md", lg: "xl" }} _expanded={{ boxShadow: "2px 2px 4px" }}
+            >
+                <Flex width="100%" direction={{ base: "column", md: "row" }} wrap="wrap" justifyContent="space-between">
+                    <Text>
+                        {job.title}
+                    </Text>
+                    <Spacer />
+                    <Text px={2}>
+                        {job.start} - {job.end}
+                    </Text>
+                    <AccordionIcon />
+                </Flex>
+            </AccordionButton>
+
+            <AccordionPanel p={4}>
+                <Text fontSize={{ base: "md", lg: "lg" }} px={4}>
+                    {job.company} | {job.place}
+                </Text>
+                <Divider mx="auto" my={1} width="95%" />
+                <Stack as={UnorderedList} ms={8}>
+                    {
+                        job.responsibilities.map(responsibility => {
+                            return (
+                                <ListItem key={nanoid()} textAlign="justify">
+                                    {responsibility}
+                                </ListItem>
+                            )
+                        })
+                    }
+                </Stack>
+            </AccordionPanel>
+        </AccordionItem>
+
+    )
+}
+
+export { ExperienceCard }
--- react/frontend/src/components/Experiences.jsx
+++ react/frontend/src/components/Experiences.jsx
@@ -1,22 +1,57 @@
-import { Heading, Text } from "@chakra-ui/react"
+import { nanoid } from "nanoid"
+import { Heading, Text, Spacer } from "@chakra-ui/react"
 import { Stack, Flex, Divider } from "@chakra-ui/react"
 import { Card, CardHeader, CardFooter, CardBody } from "@chakra-ui/react"
+import { List, UnorderedList, ListItem } from "@chakra-ui/react"
+import { Accordion, AccordionItem, AccordionButton, AccordionIcon, AccordionPanel } from "@chakra-ui/react"
+import { experiences, volunteer } from "../data/portfolio_db.json"
+import { ExperienceCard } from "./ExperienceCard"
 
 function Experiences() {
     return (
-        <Stack m={2} p={2} width="100%" bg="lightblue">
-            <Card>
+        <Stack
+            width="100%"
+        // bg="lightblue"
+        // m={2} p={2}
+        >
+            <Card as="section" border="2px solid" boxShadow="2px 2px 8px">
                 <CardHeader as={Heading} mx="auto" my={0} py={1}>
                     Experiences
                 </CardHeader>
                 <Divider className="divider" width="95%" mx="auto" my={1} />
                 <CardBody>
                     <Text>
-                        sometext
+                        Currently, I am looking for Jr. Software Developer role. Open to work on cool projects with hoping to integrate the skills I learned from my diverse work-experience.
                     </Text>
                 </CardBody>
             </Card>
-        </Stack>
+
+
+            {/* section about work */}
+            <Card as="section" border="2px solid" boxShadow="2px 2px 8px">
+                <CardHeader as={Heading} fontSize={{ base: "md", lg: "2xl" }}>
+                    Work Experience{experiences.length > 1 && `s`}
+                </CardHeader>
+                <Accordion allowToggle>
+                    {
+                        experiences.map(e => <ExperienceCard key={nanoid()} job={e} />)
+                    }
+                </Accordion>
+            </Card>
+
+            {/* section about volunteer work */}
+            <Card as="section" border="2px solid" boxShadow="2px 2px 8px">
+                <CardHeader as={Heading} fontSize={{ base: "md", lg: "2xl" }}>
+                    Volunteer Experience{volunteer.length > 1 && `s`}
+                </CardHeader>
+
+                <Accordion allowToggle>
+                    {
+                        volunteer.map(v => <ExperienceCard key={nanoid()} job={v} />)
+                    }
+                </Accordion>
+            </Card>
+        </Stack >
     )
 }
 
