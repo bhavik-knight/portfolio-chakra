2da636fa3fe519bf754c41eee73c0d3a2cacebbd
Bhavik <15388512+bhavik-knight@users.noreply.github.com15388512+bhavik-knight@users.noreply.github.com>
2023-06-08 04:13:31 -0300

    changed the boxshadow to only visible on hover

--- react/frontend/src/components/Skills.jsx
+++ react/frontend/src/components/Skills.jsx
@@ -1,6 +1,4 @@
 import { nanoid } from "nanoid"
-import { useState, useEffect } from "react"
-import { useBoolean } from "@chakra-ui/react"
 import { Heading, Text, UnorderedList } from "@chakra-ui/react"
 import { Flex, Center, Container } from "@chakra-ui/react"
 import { Card, CardHeader, CardFooter, CardBody } from "@chakra-ui/react"
@@ -8,29 +6,10 @@ import { Button, Tag } from "@chakra-ui/react"
 import { Stack, VStack, HStack } from "@chakra-ui/react"
 import { Divider, Spacer } from "@chakra-ui/react"
 import { List, ListItem } from "@chakra-ui/react"
-import { icons } from "./Icons"
 import { skills } from "../data/portfolio_db.json"
+import { ProgrammingParadigms } from "./ProgrammingParadigms"
 
 function Skills() {
-    // const web = ["python", "javascript", "html", "css", "jquery", "php", "sql", "reactjs", "chakraui", "materialui", "boostrap", "django", "flask", "postgresql", "mysql", "sqlite", "heroku", "netlify"]
-
-    // const data = ["python", "r", "sql", "numpy", "pandas", "nltk", "scikit-learn", "matplotlib", "seaborn", "plotly", "beautifulsoup"]
-
-    // const [webBtn, setWebBtn] = useBoolean(false)
-    // const [dataBtn, setDataBtn] = useBoolean(false)
-
-    // const [langs, setLangs] = useState(skills.languages)
-    // const [frameWorks, setFrameWorks] = useState(skills.frameworks)
-
-    // useEffect(() => {
-    //     webBtn && setLangs(skills.languages.filter(l => web.includes(l.toLowerCase()))) && setFrameWorks(skills.frameworks.filter(fw => web.includes(fw.toLowerCase())))
-
-    //     dataBtn && setLangs(skills.langs.filter(l => data.includes(l.toLowerCase()))) && setFrameWorks(skills.frameworks.filter(fw => data.includes(fw.toLowerCase())))
-
-    //     console.log(`wb ${webBtn}: ${langs}, ${frameWorks}`)
-    //     console.log(`data ${dataBtn}: ${langs}, ${frameWorks}`)
-
-    // }, [webBtn, dataBtn])
 
     return (
         <Stack
@@ -41,7 +20,7 @@ function Skills() {
         // bg="lightblue"
         >
             {/* introduction */}
-            <Card as="section" boxShadow="2px 2px 4px">
+            <Card as="section" _hover={{ boxShadow: "2px 2px 4px 2px" }}>
                 <CardHeader as={Heading} mx="auto" my={0} py={1}>
                     Skills
                 </CardHeader>
@@ -75,7 +54,7 @@ function Skills() {
             </Card>
 
             {/* programming languages */}
-            <Card as="section" boxShadow="2px 2px 4px">
+            <Card as="section" _hover={{ boxShadow: "2px 2px 4px 2px" }}>
                 <CardHeader as={Heading} mx="auto" my={0} py={1} fontSize={{ base: "md", lg: "2xl" }}>
                     Programming Languages
                 </CardHeader>
@@ -91,7 +70,7 @@ function Skills() {
             </Card>
 
             {/* frameworks/libraries */}
-            <Card as="section" boxShadow="2px 2px 4px">
+            <Card as="section" _hover={{ boxShadow: "2px 2px 4px 2px" }}>
                 <CardHeader as={Heading} mx="auto" my={0} py={1} fontSize={{ base: "md", lg: "2xl" }}>
                     Frameworks | Libraries
                 </CardHeader>
@@ -107,7 +86,7 @@ function Skills() {
             </Card>
 
             {/* Technologies & OS */}
-            <Card as="section" boxShadow="2px 2px 4px">
+            <Card as="section" _hover={{ boxShadow: "2px 2px 4px 2px" }}>
                 <CardHeader as={Heading} mx="auto" my={0} py={1} fontSize={{ base: "md", lg: "2xl" }}>
                     Technologies | Operating Systems
                 </CardHeader>
@@ -130,24 +109,8 @@ function Skills() {
                 </CardBody>
             </Card>
 
-            {/* project management */}
-            <Card as="section" boxShadow="2px 2px 4px">
-                <CardHeader as={Heading} mx="auto" my={0} py={1} fontSize={{ base: "md", lg: "2xl" }}>
-                    Project Management
-                </CardHeader>
-                <CardBody as={Flex} wrap="wrap" gap={2} justifyContent="space-evenly">
-                    {
-                        skills.management.map(mgmt => {
-                            return (
-                                <Tag key={nanoid()}>{mgmt}</Tag>
-                            )
-                        })
-                    }
-                </CardBody>
-            </Card>
-
             {/* applications */}
-            <Card as="section" boxShadow="2px 2px 4px">
+            <Card as="section" _hover={{ boxShadow: "2px 2px 4px 2px" }}>
                 <CardHeader as={Heading} mx="auto" my={0} py={1} fontSize={{ base: "md", lg: "2xl" }}>
                     Applications | Database | Cloud
                 </CardHeader>
@@ -162,68 +125,24 @@ function Skills() {
                 </CardBody>
             </Card>
 
-            {/* programming paradigms */}
-            <Card as="section" boxShadow="2px 2px 4px">
+            {/* project management */}
+            <Card as="section" _hover={{ boxShadow: "2px 2px 4px 2px" }}>
                 <CardHeader as={Heading} mx="auto" my={0} py={1} fontSize={{ base: "md", lg: "2xl" }}>
-                    Programming Paradigms
+                    Project Management
                 </CardHeader>
-                <Divider mx="auto" width="95%" my={1} />
-                <CardBody>
-                    <UnorderedList ps={4}>
-                        <ListItem>
-                            Traditionally we all learn procedural programming and OOP concepts. However, I am familiar with declaratice paradigms too.
-                        </ListItem>
-                        <ListItem>
-                            I leverage functional programming paradigm (map, filter, reduce, lambda functions) of Python and JS where I can.
-                        </ListItem>
-                        <ListItem>
-
-                            In Artificial Intelligence, I learned First Order Logic (FOL), which in addition to the facts, can express object, relations and functions.
-                        </ListItem>
-                        <ListItem>
-                            To perform the CRUD Operations on the databases, I use SQL. Thus, conceptually I have knowledge of all programming paradigms.
-                        </ListItem>
-                    </UnorderedList>
+                <CardBody as={Flex} wrap="wrap" gap={2} justifyContent="space-evenly">
+                    {
+                        skills.management.map(mgmt => {
+                            return (
+                                <Tag key={nanoid()}>{mgmt}</Tag>
+                            )
+                        })
+                    }
                 </CardBody>
+            </Card>
 
-                <Divider mx="auto" width="95%" my={1} />
-
-                <CardFooter
-                    as={Stack}
-                    direction={{ base: "column", lg: "row" }}
-                    justifyContent={{ base: "center", md: "space-evenly" }}
-                >
-                    <Card
-                        p={1}
-                        textAlign="center"
-                        _hover={{ boxShadow: "1px 1px 2px" }}
-                        width={{ base: "100%", lg: "25%" }}
-                    >
-                        <CardHeader pb={0} textDecoration="underline">Imperative</CardHeader>
-                        <CardBody as={List}>
-                            <ListItem>Procedural</ListItem>
-                            <ListItem>Object Oriented</ListItem>
-                            <ListItem>Structural</ListItem>
-                        </CardBody>
-                    </Card>
-
-                    <Divider orientation={{ base: "horizontal", md: "vertical" }} />
-
-                    <Card
-                        p={1}
-                        textAlign="center"
-                        _hover={{ boxShadow: "1px 1px 2px" }}
-                        width={{ base: "100%", lg: "25%" }}
-                    >
-                        <CardHeader pb={0} textDecoration="underline">Declarative</CardHeader>
-                        <CardBody as={List}>
-                            <ListItem>Functional</ListItem>
-                            <ListItem>Logical</ListItem>
-                            <ListItem>Database</ListItem>
-                        </CardBody>
-                    </Card>
-                </CardFooter>
-            </Card >
+            {/* programming paradigms */}
+            {/* <ProgrammingParadigms /> */}
         </Stack >
     )
 }
