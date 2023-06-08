eec89b2d1aaf25ec9d8c4c347d03f27c27b23d89
Bhavik <15388512+bhavik-knight@users.noreply.github.com15388512+bhavik-knight@users.noreply.github.com>
2023-06-08 00:17:15 -0300

    trying to render pdf using react-pdf

--- react/frontend/src/components/CertificateCardCarousel.jsx
+++ react/frontend/src/components/CertificateCardCarousel.jsx
@@ -1,29 +1,90 @@
 
-import { chakra } from "@chakra-ui/react"
+import { IconButton, chakra } from "@chakra-ui/react"
 import { nanoid } from "nanoid"
-import { Wrap, Box, Stack, Flex, Spacer, Container, Center } from "@chakra-ui/react"
-import { Button, Image, Text, Heading } from "@chakra-ui/react"
+import { Wrap, Box, Stack, VStack, HStack, Flex, Spacer, Container, Center } from "@chakra-ui/react"
+import { Button, ButtonGroup, Image, Text, Heading } from "@chakra-ui/react"
 import { UnorderedList, List, ListItem } from "@chakra-ui/react"
 import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react"
-import { useColorModeValue } from "@chakra-ui/react"
-import { Carousel } from "react-responsive-carousel"
-import "react-responsive-carousel/lib/styles/carousel.min.css"
+import { useState, useRef, useEffect } from "react"
+import { ArrowLeftIcon, ArrowRightIcon } from "@chakra-ui/icons"
+import { Document, Page, pdfjs } from "react-pdf"
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    'pdfjs-dist/build/pdf.worker.min.js',
+    import.meta.url,
+).toString();
 
 function CertificateCardCarousel({ details }) {
 
+    // to keep track of the current carousel
+    const length = details.length
+    const [current, setCurrent] = useState(0)
+
+    function handlePrevClick() {
+        current < 1 && setCurrent(current => current + length)
+        setCurrent(current => (current - 1) % length)
+    }
+
+    function handleNextClick() {
+        setCurrent(current => (current + 1) % length)
+    }
+
+    function handlePageClick(index) {
+        setCurrent(index)
+    }
+
+    // to keep track of the number of buttons in the paginations
+    const siblings = 2
+    const numberOfPaginationButtons = siblings * 2 + 1
+    function showButtons(active) {
+        if (length < numberOfPaginationButtons) {
+            return [...Array(length).keys()]
+        }
+
+        if (active < siblings) {
+            active = siblings
+        } else if (active >= length - siblings) {
+            active = length - siblings - 1
+        }
+
+        let newArray = [...Array(numberOfPaginationButtons).keys()].map(i => i - siblings + active)
+        return newArray
+    }
+
+    // autoplay of carousel cards
+    const timeRef = useRef(0)
+    useEffect(() => {
+        if (timeRef.current) {
+            clearTimeout(timeRef.current)
+        }
+
+        // trigger next button click after 4 seconds
+        timeRef.current = setTimeout(() => handleNextClick(), 4000)
+
+        return () => clearTimeout(timeRef.current)
+    }, [handleNextClick])
+
+
+    // styles for cards & carousel
+    const cardWidth = 1024;
+    const cardHeight = 768;
+
+
+    // the certificates data cards preparation
     const card =
         details.map(c => {
             return (
                 <Card
                     key={nanoid()}
-                    border="1px solid"
-                    p={2} m={2}
-                    boxSize="90%"
+                    m={2}
                     mx="auto"
+                    p={2}
+                    border="1px dashed"
                     _hover={{ cursor: "pointer", boxShadow: "2px 2px 8px" }}
                 >
                     <CardHeader
-                        // bg="blue.200"
                         as={Stack}
                         my={0}
                         direction={{ base: "column", lg: "row" }}
@@ -50,47 +111,92 @@ function CertificateCardCarousel({ details }) {
                         }
                     </CardHeader>
                     <CardBody as={Center} my={0}>
-                        <chakra.embed
-                            width={{ base: "300px", lg: "1024px" }}
-                            height={{ base: "200px", lg: "768px" }}
+                        {/* <chakra.embed
+                            width={{ base: `256px`, lg: `${cardWidth * 0.8}px` }}
+                            height={{ base: `64px`, lg: `${512}px` }}
                             src={c.certImg}
-                            type="application/pdf"
-                        />
+                        /> */}
+
+                        <Document file={c.certImg}>
+                            <Page pageNumber={1} />
+                        </Document>
+
                     </CardBody>
-                    <CardFooter>
-                        <Text>
-                            {c.learnings}
-                        </Text>
+                    <CardFooter as={Text} textAlign="justify">
+                        {c.learnings}
                     </CardFooter>
                 </Card>
             )
         })
 
-
-    const dummyCard = details.map(c => {
-        return (
-            <Card key={nanoid()} border="1px solid">
-                <CardHeader>
-                    {c.certName}
-                </CardHeader>
-                <CardBody>
-                    {c.certImg}
-                </CardBody>
-            </Card>
-        )
-    })
-
     return (
-        <Container
+        <Box
             border="1px solid red"
+            p={2}
         >
-            <Carousel
-            >
+            {/* show current & total number of certificates */}
+            <Center as={Text}>{current + 1} of {length}</Center>
+
+            {/* middle-part: left-arrow = card = right-arrow */}
+            <HStack justifyContent="center">
+                <IconButton
+                    aria-label="left-arrow-button"
+                    icon={<ArrowLeftIcon />}
+                    onClick={handlePrevClick}
+                />
+
+                {/* carousel part */}
+                <Wrap
+                    overflow="hidden"
+                    width={`${cardWidth}px`}
+                    height={`${cardHeight}px`}
+                    border="1px solid"
+                    borderRadius="0.5em"
+                    alignItems="center"
+                    justifyContent="center"
+                >
+                    <Center
+                        className="certificates-card-container"
+                        width={`${cardWidth * length}px`}
+                    >
+                        {
+                            card.map((_, index) => {
+                                return (
+                                    <Box key={nanoid()} width={cardWidth}>
+                                        {card[current]}
+                                    </Box>
+                                )
+                            })
+                        }
+                    </Center>
+                </Wrap>
+
+                <IconButton
+                    aria-label="right-arrow-button"
+                    icon={<ArrowRightIcon />}
+                    onClick={handleNextClick}
+                />
+
+            </HStack >
+
+            {/* pagination area */}
+            < Center as={ButtonGroup} spacing={2} >
                 {
-                    dummyCard
+                    showButtons(current).map((b, index) => {
+                        return (
+                            <Button
+                                key={nanoid()}
+                                className="dots"
+                                onClick={() => handlePageClick(b)}
+                                isActive={b === current}
+                            >
+                                {b + 1}
+                            </Button>
+                        )
+                    })
                 }
-            </Carousel>
-        </Container>
+            </Center >
+        </Box >
     )
 }
 
