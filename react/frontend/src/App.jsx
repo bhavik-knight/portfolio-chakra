018b78167f1b140b69f88ea9c852b938550e93d0
Bhavik <15388512+bhavik-knight@users.noreply.github.com15388512+bhavik-knight@users.noreply.github.com>
2023-06-08 00:16:18 -0300

    fixed indentation inconsistency - 4 spaces

--- react/frontend/src/App.jsx
+++ react/frontend/src/App.jsx
@@ -15,108 +15,108 @@ import { ResponsiveIcons } from "./components/ResponsiveIcons"
 
 function App() {
 
-  // color mode toggle; check the local storage on each color mode change
-  const { colorMode, toggleColorMode } = useColorMode()
-  useEffect(() => {
-    // console.log(`check the local storage: ${JSON.stringify(localStorage)}`)
-  }, [colorMode])
+    // color mode toggle; check the local storage on each color mode change
+    const { colorMode, toggleColorMode } = useColorMode()
+    useEffect(() => {
+        // console.log(`check the local storage: ${JSON.stringify(localStorage)}`)
+    }, [colorMode])
 
-  // pages in the portfolio
-  const pages = {
-    "home": {
-      "icon": ResponsiveIcons.home,
-      "page": <Home />,
-    },
+    // pages in the portfolio
+    const pages = {
+        "home": {
+            "icon": ResponsiveIcons.home,
+            "page": <Home />,
+        },
 
-    "skills": {
-      "icon": ResponsiveIcons.skills,
-      "page": <Skills />,
-    },
+        "skills": {
+            "icon": ResponsiveIcons.skills,
+            "page": <Skills />,
+        },
 
-    "projects": {
-      "icon": ResponsiveIcons.projects,
-      "page": <Projects />,
-    },
+        "projects": {
+            "icon": ResponsiveIcons.projects,
+            "page": <Projects />,
+        },
 
-    "education": {
-      "icon": ResponsiveIcons.education,
-      "page": <Education />,
-    },
+        "education": {
+            "icon": ResponsiveIcons.education,
+            "page": <Education />,
+        },
 
-    "experiences": {
-      "icon": ResponsiveIcons.experiences,
-      "page": <Experiences />,
-    },
+        "experiences": {
+            "icon": ResponsiveIcons.experiences,
+            "page": <Experiences />,
+        },
 
-    "certificates": {
-      "icon": ResponsiveIcons.certificates,
-      "page": <Certificates />
+        "certificates": {
+            "icon": ResponsiveIcons.certificates,
+            "page": <Certificates />
+        }
     }
-  }
 
-  // to load a particular page on button click
-  const [currentPage, setCurrentPage] = useState(localStorage.getItem("currentPage") || "home")
-  function handleSelectPage(event) {
-    console.log(`app - side bar button click event: ${event}`)
-    setCurrentPage(event.currentTarget.name)
-    console.log(`app - ${currentPage}`)
-  }
-  useEffect(() => {
-    localStorage.setItem("currentPage", currentPage)
-    document.title = `Bhavik | ${currentPage[0].toUpperCase() + currentPage.substring(1)}`
-    window.scrollTo(0, 0)
-  }, [currentPage])
+    // to load a particular page on button click
+    const [currentPage, setCurrentPage] = useState(localStorage.getItem("currentPage") || "home")
+    function handleSelectPage(event) {
+        console.log(`app - side bar button click event: ${event}`)
+        setCurrentPage(event.currentTarget.name)
+        console.log(`app - ${currentPage}`)
+    }
+    useEffect(() => {
+        localStorage.setItem("currentPage", currentPage)
+        document.title = `Bhavik | ${currentPage[0].toUpperCase() + currentPage.substring(1)}`
+        window.scrollTo(0, 0)
+    }, [currentPage])
 
-  // this is the function that will render the DOM
-  return (
-    <Flex className="container" flexDirection="column" flexWrap="wrap">
-      <Header
-        colorMode={colorMode}
-        changeColorMode={() => toggleColorMode()}
-        title={currentPage}
-      />
-      {/* middle part */}
-      <Flex
-        minH="100vh"
-        flexdirection={{ base: "column", lg: "row" }}
-        flexWrap={{ base: "wrap", lg: "nowrap" }}
-      >
+    // this is the function that will render the DOM
+    return (
+        <Flex className="container" flexDirection="column" flexWrap="wrap">
+            <Header
+                colorMode={colorMode}
+                changeColorMode={() => toggleColorMode()}
+                title={currentPage}
+            />
+            {/* middle part */}
+            <Flex
+                minH="100vh"
+                flexdirection={{ base: "column", lg: "row" }}
+                flexWrap={{ base: "wrap", lg: "nowrap" }}
+            >
 
-        <Flex
-          // bg="lightgreen"
-          className="sidenav"
-          as="aside"
-          width={{ base: "100%", lg: "15%" }}
-          flexDirection={{ base: "row", lg: "column" }}
-          flexWrap={{ base: "wrap", lg: "nowrap" }}
-          justifyContent={{ base: "center", lg: "flex-start" }}
-          pt={2}
-          gap={2}
-        >
-          <Sidenav
-            pages={pages}
-            activePage={currentPage}
-            selectPage={handleSelectPage}
-          />
-        </Flex>
-        <Flex
-          // bg="lightgreen"
-          className="content"
-          as="main"
-          px={{ base: 0, lg: "4px" }}
-          width="70%"
-          minH="fit-content"
-          mx="auto"
-          justify="center"
-        >
-          {pages[currentPage].page}
-        </Flex>
-      </Flex>
+                <Flex
+                    // bg="lightgreen"
+                    className="sidenav"
+                    as="aside"
+                    width={{ base: "100%", lg: "15%" }}
+                    flexDirection={{ base: "row", lg: "column" }}
+                    flexWrap={{ base: "wrap", lg: "nowrap" }}
+                    justifyContent={{ base: "center", lg: "flex-start" }}
+                    pt={2}
+                    gap={2}
+                >
+                    <Sidenav
+                        pages={pages}
+                        activePage={currentPage}
+                        selectPage={handleSelectPage}
+                    />
+                </Flex>
+                <Flex
+                    // bg="lightgreen"
+                    className="content"
+                    as="main"
+                    px={{ base: 0, lg: "4px" }}
+                    width="70%"
+                    minH="fit-content"
+                    mx="auto"
+                    justify="center"
+                >
+                    {pages[currentPage].page}
+                </Flex>
+            </Flex>
 
-      {/*  footer */}
-      <Footer />
-    </Flex >
-  )
+            {/*  footer */}
+            <Footer />
+        </Flex >
+    )
 }
 
 export { App }
