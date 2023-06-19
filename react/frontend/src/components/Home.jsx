fde9d41bf14df368b89a05b758927f6516e88a32
Bhavik <15388512+bhavik-knight@users.noreply.github.com15388512+bhavik-knight@users.noreply.github.com>
2023-06-19 20:01:54 -0300

    made home-page-skills card modular

--- react/frontend/src/components/Home.jsx
+++ react/frontend/src/components/Home.jsx
@@ -6,6 +6,25 @@ import { Divider, Spacer, Button } from "@chakra-ui/react"
 import { Card, CardHeader, CardFooter, CardBody } from "@chakra-ui/react"
 
 function Home() {
+
+    function getHomeSkillsCard(skillName, details, color) {
+        return (
+            <Card
+                as={Center}
+                borderBottom={`1px solid ${color}`}
+                _hover={{ boxShadow: `1px 1px 16px ${color}` }}
+                width={{ base: "100%", md: "80%", xl: "40%" }}
+                height={{ base: "100px", md: "200px" }}
+                className="homeSkills"
+            >
+                <Text as="strong" className="homeSkillName">{skillName}</Text>
+                <CardBody className="homeSkillDetails" textAlign="justify">
+                    <Text noOfLines={[2, 4, 8]}> {details} </Text>
+                </CardBody>
+            </Card>
+        )
+    }
+
     return (
         <Stack
             p={2}
@@ -48,80 +67,49 @@ function Home() {
                     <Flex
                         width={{ base: "100%", xl: "40%" }}
                         boxSize={{ base: "base", md: "md", lg: "lg" }}
+                        as={Container}
                         mx="auto"
                     >
                         <Image src="./logos/bhavik_sticker.png" objectFit="contain" alt="bhavik sticker" />
                     </Flex>
-                    <Flex
+                    <Stack
                         width={{ base: "100%", xl: "60%" }}
-                        direction={{ base: "column", xl: "row" }}
+                        flexDirection={{ base: "column", xl: "row" }}
                         justifyContent="space-around"
                         alignItems="center"
                         wrap="wrap"
                         p={0}
                     >
-                        <Card
-                            as={Center}
-                            borderBottom="1px solid red"
-                            _hover={{ boxShadow: "2px 2px 8px red" }}
-                            width={{ base: "100%", md: "80%", xl: "40%" }}
-                            height={{ base: "base", md: "200px" }}
-                            className="homeSkills"
-                        >
-                            <Text as="strong" className="homeSkillName">Full Stack Developement</Text>
-                            <CardBody className="homeSkillDetails" textAlign="justify">
-                                <Text noOfLines={[2, 4, 8]}>
-                                    Worked on numerous Full Stack project using Django, Flask, PHP, HTML, CSS, JavaScript, jQuery, Bootstrap, ReactJS, ChakraUI, MySQL, Postgreq and hosted the projects on Heroku and AWS using Apache server.
-                                </Text>
-                            </CardBody>
-                        </Card>
-                        <Card
-                            as={Center}
-                            borderBottom="1px solid green"
-                            _hover={{ boxShadow: "2px 2px 8px green" }}
-                            width={{ base: "100%", md: "80%", xl: "40%" }}
-                            height={{ base: "base", md: "200px" }}
-                            className="homeSkills"
-                        >
-                            <Text as="strong" className="homeSkillName">Data Analyst</Text>
-                            <CardBody className="homeSkillDetails" textAlign="justify">
-                                <Text noOfLines={[2, 4, 8]}>
-                                    Worked on the projects executing ETL operations to create data-pipelines to do EDA, and communicated the results using the tools like Numpy, Pandas, Plotly, Ggplot, etc.
-                                </Text>
-                            </CardBody>
-                        </Card>
+                        {
+                            getHomeSkillsCard(
+                                "Full Stack Development",
+                                "Worked on numerous Full Stack project using Django, Flask, PHP, HTML, CSS, JavaScript, jQuery, Bootstrap, ReactJS, ChakraUI, MySQL, Postgreq and hosted the projects on Heroku and AWS using Apache server.",
+                                "red"
+                            )
+                        }
+                        {
+                            getHomeSkillsCard(
+                                "Data Analyst",
+                                "Worked on the projects executing ETL operations to create data-pipelines to do EDA, and communicated the results using the tools like Numpy, Pandas, Plotly, Ggplot, etc.",
+                                "green"
+                            )
+                        }
+                        {
+                            getHomeSkillsCard(
+                                "Teamwork",
+                                "Completed several projects as a team where I contributed during each stage. Experience as a scrum master as well as scribe using the tools like project management tools like Jira, Trello, etc.",
+                                "blue"
+                            )
+                        }
+                        {
+                            getHomeSkillsCard(
+                                "Problem Solving",
+                                "Leverage critical thinking, analytical skills, logic and reasoning, in combination with tools like Google, Stackoverflow, chatGPT, and when required help of experienced colleauge to solve the problems.",
+                                "yellow"
+                            )
 
-                        <Card
-                            as={Center}
-                            borderBottom="1px solid blue"
-                            _hover={{ boxShadow: "2px 2px 8px blue" }}
-                            width={{ base: "100%", md: "80%", xl: "40%" }}
-                            height={{ base: "base", md: "200px" }}
-                            className="homeSkills"
-                        >
-                            <Text as="strong" className="homeSkillName">Teamwork</Text>
-                            <CardBody className="homeSkillDetails" textAlign="justify">
-                                <Text noOfLines={[2, 4, 8]}>
-                                    Completed several projects as a team where I contributed during each stage. Experience as a scrum master as well as scribe using the tools like project management tools like Jira, Trello, etc.
-                                </Text>
-                            </CardBody>
-                        </Card>
-                        <Card
-                            as={Center}
-                            borderBottom="1px solid yellow"
-                            _hover={{ boxShadow: "2px 2px 8px yellow" }}
-                            width={{ base: "100%", md: "80%", xl: "40%" }}
-                            height={{ base: "base", md: "200px" }}
-                            className="homeSkills"
-                        >
-                            <Text as="strong" className="homeSkillName">Problem Solving</Text>
-                            <CardBody className="homeSkillDetails" textAlign="justify">
-                                <Text noOfLines={[2, 4, 8]}>
-                                    Leverage critical thinking, analytical skills, logic and reasoning, in combination with tools like Google, Stackoverflow, chatGPT, and when required help of experienced colleauge to solve the problems.
-                                </Text>
-                            </CardBody>
-                        </Card>
-                    </Flex>
+                        }
+                    </Stack>
                 </Stack>
 
             </Card >
