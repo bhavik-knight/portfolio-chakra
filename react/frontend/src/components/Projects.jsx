efcdb1f0098839a9558198a99158ba759b23ca32
Bhavik <15388512+bhavik-knight@users.noreply.github.com15388512+bhavik-knight@users.noreply.github.com>
2023-07-01 22:04:15 -0300

    added the spinner for loading while fetching the data

--- react/frontend/src/components/Projects.jsx
+++ react/frontend/src/components/Projects.jsx
@@ -1,8 +1,8 @@
 import "./Projects.css"
 import { useState, useEffect } from "react"
-import { Divider, ListIcon, List, ListItem, TagRightIcon } from "@chakra-ui/react"
+import { Divider, ListIcon, List, ListItem, TagRightIcon, Spinner } from "@chakra-ui/react"
 import { Heading, Text } from "@chakra-ui/react"
-import { Flex, Stack, VStack, HStack, } from "@chakra-ui/react"
+import { Flex, Stack, Center, VStack, HStack, } from "@chakra-ui/react"
 import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react"
 import { Tabs, Tab, TabPanels, TabPanel, TabList, } from "@chakra-ui/react"
 import { nanoid } from "nanoid"
@@ -20,13 +20,19 @@ const textFontStyle = {
 
 function Projects() {
     const [projects, setProjects] = useState([])
+    const [loading, setLoading] = useState(true)
+
     // fetch all projects data from the firebase db
     useEffect(() => {
+        setLoading(true)
         getData("projects")
             .then(data => setProjects(data))
             .catch(error => console.log(error.message))
     }, [])
 
+    useEffect(() => {
+        setLoading(projects.length === 0)
+    }, [projects])
 
 
     // domains
@@ -36,65 +42,76 @@ function Projects() {
     const [tabIndex, setTabIndex] = useState(0)
 
     return (
-        <Stack p={{ base: 0, lg: 2 }} gap={{ base: 1, lg: 2 }} w="100%">
-            {/* some text */}
-            <Card as="section" _hover={{ boxShadow: "4px 4px 16px" }}>
-                <CardHeader as={Heading} mx="auto" my={1} py={1}>
-                    Projects
-                </CardHeader>
+        loading ?
+            <Stack
+                p={{ base: 0, lg: 2 }}
+                gap={{ base: 1, lg: 2 }}
+                as={Center}
+                minH="100vh"
+                w="100%"
+            >
+                <Spinner size="xl" />
+            </Stack >
+            :
+            <Stack p={{ base: 0, lg: 2 }} gap={{ base: 1, lg: 2 }} w="100%">
+                {/* some text */}
+                <Card as="section" _hover={{ boxShadow: "4px 4px 16px" }}>
+                    <CardHeader as={Heading} mx="auto" my={1} py={1}>
+                        Projects
+                    </CardHeader>
 
-                <Divider width="95%" mx="auto" my={1} />
+                    <Divider width="95%" mx="auto" my={1} />
 
-                <CardBody>
-                    <Stack as={List} spacing={2} {...textFontStyle}>
-                        <ListItem>
-                            I demonstrated diverse programming skills through numerous projects in the fields of Web Development, Data Analytics, Data Science, and Machine Learning.
-                        </ListItem>
-                        <ListItem>
-                            While I developed most of the projects independently, I also actively contributed to group projects, demonstrating my skills and depth of knowledge in the core domain.
-                        </ListItem>
-                    </Stack>
-                </CardBody>
-            </Card>
+                    <CardBody>
+                        <Stack as={List} spacing={2} {...textFontStyle}>
+                            <ListItem>
+                                I demonstrated diverse programming skills through numerous projects in the fields of Web Development, Data Analytics, Data Science, and Machine Learning.
+                            </ListItem>
+                            <ListItem>
+                                While I developed most of the projects independently, I also actively contributed to group projects, demonstrating my skills and depth of knowledge in the core domain.
+                            </ListItem>
+                        </Stack>
+                    </CardBody>
+                </Card>
 
 
-            <Card as="section" _hover={{ boxShadow: "4px 4px 16px" }}>
-                {/* tabs for different types of project */}
-                <Tabs
-                    isFitted
-                    isLazy
-                    p={2}
-                    my={2}
-                    variant="enclosed"
-                    size={{ base: "base", lg: "md" }}
-                    orientation={{ base: "vertical", lg: "horizontal" }}
-                    onChange={(index) => setTabIndex(index)}
-                >
-                    {/* tab titles */}
-                    <TabList className="tabLabel" width={{ base: "100%", lg: "90%" }} mx="auto" fontSize={{ base: "0.9em", md: "0.95em", lg: "1em" }}>
-                        <Tab _selected={{ bg: "red", color: "black" }} _hover={{ border: `1px solid red` }}>Web Dev</Tab>
-                        <Tab _selected={{ bg: "green", color: "white" }} _hover={{ border: `1px solid green` }}>Data</Tab>
-                        <Tab _selected={{ bg: "blue", color: "white" }} _hover={{ border: `1px solid blue` }}>ML/AI</Tab>
-                        <Tab _selected={{ bg: "yellow", color: "black" }} _hover={{ border: `1px solid yellow` }}>Game Dev</Tab>
-                    </TabList>
+                <Card as="section" _hover={{ boxShadow: "4px 4px 16px" }}>
+                    {/* tabs for different types of project */}
+                    <Tabs
+                        isFitted
+                        isLazy
+                        p={2}
+                        my={2}
+                        variant="enclosed"
+                        size={{ base: "base", lg: "md" }}
+                        orientation={{ base: "vertical", lg: "horizontal" }}
+                        onChange={(index) => setTabIndex(index)}
+                    >
+                        {/* tab titles */}
+                        <TabList className="tabLabel" width={{ base: "100%", lg: "90%" }} mx="auto" fontSize={{ base: "0.9em", md: "0.95em", lg: "1em" }}>
+                            <Tab _selected={{ bg: "red", color: "black" }} _hover={{ border: `1px solid red` }}>Web Dev</Tab>
+                            <Tab _selected={{ bg: "green", color: "white" }} _hover={{ border: `1px solid green` }}>Data</Tab>
+                            <Tab _selected={{ bg: "blue", color: "white" }} _hover={{ border: `1px solid blue` }}>ML/AI</Tab>
+                            <Tab _selected={{ bg: "yellow", color: "black" }} _hover={{ border: `1px solid yellow` }}>Game Dev</Tab>
+                        </TabList>
 
-                    {/* tab content */}
-                    <TabPanels>
-                        {
-                            projectTypes.map(type =>
-                                <TabPanel as={Flex} key={nanoid()} justifyContent="space-evenly" flexWrap="wrap" px={0}>
-                                    {
-                                        projects
-                                            .filter(p => p.type === type)
-                                            .map(p => <ProjectCard key={nanoid()} project={p} />)
-                                    }
-                                </TabPanel>
-                            )
-                        }
-                    </TabPanels>
-                </Tabs>
-            </Card>
-        </Stack>
+                        {/* tab content */}
+                        <TabPanels>
+                            {
+                                projectTypes.map(type =>
+                                    <TabPanel as={Flex} key={nanoid()} justifyContent="space-evenly" flexWrap="wrap" px={0}>
+                                        {
+                                            projects
+                                                .filter(p => p.type === type)
+                                                .map(p => <ProjectCard key={nanoid()} project={p} />)
+                                        }
+                                    </TabPanel>
+                                )
+                            }
+                        </TabPanels>
+                    </Tabs>
+                </Card>
+            </Stack>
     )
 }
 
